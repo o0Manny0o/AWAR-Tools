8350ebca321011a7167d034abfc01a83852b72e4
o0Manny0o <moritz.wach@t-online.de>
2024-11-17 15:04:06 +0100

    Implement delete key

--- tools/internationalisation/editor/components/MenuItem.tsx
+++ tools/internationalisation/editor/components/MenuItem.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import { JSONValue } from "https://deno.land/std@0.173.0/encoding/jsonc.ts";
 import { JSONObject } from "../shared/types.ts";
 import { Menu } from "./Menu.tsx";
+import { DeleteKey } from "./DeleteKey.tsx";
 
 interface MenuProps {
     tKey: string;
@@ -22,8 +23,10 @@ export function MenuItem({
         JSON.parse(localStorage.getItem(tKey) ?? "false"),
     );
 
-    function selectItem(item: string, parents: string[]) {
-        onItemSelected([...parents, item].join("."));
+    const fullKey = [...parents, tKey].join(".");
+
+    function selectItem() {
+        onItemSelected(fullKey);
     }
 
     const hasChildren = (item: JSONValue) => typeof item === "object";
@@ -36,49 +39,55 @@ export function MenuItem({
     }, [childrenExpanded]);
 
     return (
-        <li key={tKey} className="group">
-            <div className="flex items-center gap-2">
-                <div
-                    onClick={() => selectItem(tKey, parents)}
-                    className={
-                        "p-1 flex gap-2 items-center flex-1 cursor-pointer rounded-sm hover:bg-orange-500 transition-colors " +
-                        (tKey === selectedKey?.split(".")[0]
-                            ? "shadow-sm bg-orange-300 shadow-orange-600 dark:bg-orange-600 dark:shadow-orange-300"
-                            : "")
-                    }
-                >
-                    {hasChildren(value) ? (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            className="size-4"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
-                            />
-                        </svg>
-                    ) : (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            className="size-4"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
-                            />
-                        </svg>
-                    )}
-                    {tKey}
+        <li key={tKey}>
+            <div className="group flex items-center gap-2">
+                <div className="flex items-center gap-2 flex-1">
+                    <div
+                        onClick={() => selectItem()}
+                        className={
+                            "p-1 flex gap-2 items-center flex-1 cursor-pointer rounded-sm hover:bg-orange-500 transition-colors " +
+                            (tKey === selectedKey?.split(".")[0]
+                                ? "shadow-sm bg-orange-300 shadow-orange-600 dark:bg-orange-600 dark:shadow-orange-300"
+                                : "")
+                        }
+                    >
+                        {hasChildren(value) ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                className="size-4"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                className="size-4"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                                />
+                            </svg>
+                        )}
+                        {tKey}
+                    </div>
+                    <DeleteKey
+                        langKey={fullKey}
+                        className="group-hover:opacity-100 opacity-0 transition p-1 cursor-pointer rounded-sm hover:bg-orange-500 ease-out"
+                    />
                 </div>
                 {hasChildren(value) && (
                     <button
--- tools/internationalisation/editor/routes/api/translations.ts
+++ tools/internationalisation/editor/routes/api/translations.ts
@@ -1,7 +1,6 @@
 import { Handlers } from "$fresh/server.ts";
 import { loadTranslationFiles, saveJSON } from "../../shared/loader.ts";
-import { has, set } from "lodash";
-import { writeJsonSync } from "jsonfile";
+import { has, set, unset } from "lodash";
 
 export const handler: Handlers = {
     async POST(req, ctx) {
@@ -33,6 +32,32 @@ export const handler: Handlers = {
             }
         }
         return new Response(`This key already exists`, { status: 400 });
+    },
+    async DELETE(req, ctx) {
+        const requestData = await req.json();
+        if (!requestData.key) {
+            return new Response(`No Key provided`, {
+                status: 400,
+            });
+        }
+        const key = requestData.key.toLowerCase();
+
+        const languages = await loadTranslationFiles();
+        const defaultLang = languages.find((l) => l.language == "en");
+        if (!defaultLang) {
+            return new Response(`Default language not found`, {
+                status: 404,
+            });
+        }
+        unset(defaultLang.json, key, key);
+        try {
+            saveJSON("en", defaultLang.json);
+            return new Response(JSON.stringify(defaultLang.json));
+        } catch (e) {
+            return new Response(`Writing json file failed`, {
+                status: 500,
+            });
+        }
     },
     // async PATCH(req, ctx) {
     //     const form = await req.formData();
